6a51f2a765d420450282253b6d4af146b78e645c
u16rogue <u16rogue@waifu.club>
2022-08-24 21:43:00 +0800

    math utils, player target mouse point, separate sprite

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const DEBUG = true;
+
 const $ = (id, action = null) =>
 {
     let obj = document.getElementById(id) ?? document.getElementsByClassName(id);
@@ -22,7 +24,15 @@ let PLAYER_CENTER_X = PLAYER_SPRITE_WIDTH  / 2;
 let PLAYER_CENTER_Y = PLAYER_SPRITE_HEIGHT / 2;
 
 let keys = {};
-let global_interval = 0;
+
+let state = {
+    interval : 0,
+    mouse    : {
+        x       : 0,
+        y       : 0,
+        capture : false
+    }
+};
 
 let canvas = {
     element : null,
@@ -34,8 +44,43 @@ const util_in_canvas_bound = (x, y) =>
     return x >= 0 && x <= canvas.element.width && y >= 0 && y <= canvas.element.height;
 };
 
+const util_math_distance = (p1, p2) =>
+{
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+};
+
+const util_math_normalize_towards = (p1, p2) =>
+{
+    const dist = util_math_distance(p2, p1);
+    return {
+        x: (p2.x - p1.x) / dist,
+        y: (p2.y - p1.y) / dist
+    };
+};
+
+const util_math_normalize = (p) =>
+{
+    const n = Math.sqrt(Math.pow(p.x, 2) + Math.pow(p.y, 2));
+    return {
+       x : p.x / n,
+       y : p.y / n 
+    };
+};
+
+const util_math_forward_towards = (p1, p2, distance) =>
+{
+    const vun = util_math_normalize_towards(p1, p2);
+    return {
+        x : p1.x + (vun.x * distance),
+        y : p1.y + (vun.y * distance)
+    };
+};
+
 let player = {
-    sprite : new Image(),
+    sprite : {
+        base : new Image(),
+        eyes : new Image()
+    },
     x      : 0,
     y      : 0,
     speed  : 100,
@@ -43,11 +88,42 @@ let player = {
     min_speed : 80,
     max_speed : 300,
     last_move : 0,
-    accelerate_time_ms : 1000,
+    accelerate_time_ms : 600,
 
     render : () =>
     {
-        canvas.context.drawImage(player.sprite, player.x - PLAYER_CENTER_X, player.y - PLAYER_CENTER_Y);
+        let p_abs = {
+            x : player.x - PLAYER_CENTER_X,
+            y : player.y - PLAYER_CENTER_Y
+        };
+
+        canvas.context.drawImage(player.sprite.base, p_abs.x, p_abs.y);
+        canvas.context.drawImage(player.sprite.eyes, p_abs.x, p_abs.y);
+
+        // DEBUG
+        if (DEBUG)
+        {
+            canvas.context.fillStyle = "rgb(255, 255, 255)";
+            canvas.context.strokeStyle = "rgb(255, 255, 255)";
+
+            /*
+            canvas.context.beginPath();
+            canvas.context.moveTo(player.x, player.y);
+            const test_dir = util_math_forward_towards(player, state.mouse, 200);
+            canvas.context.lineTo(test_dir.x, test_dir.y);
+            canvas.context.closePath();
+            canvas.context.stroke();
+            */
+            canvas.context.fillText(
+                "DEBUG >> Speed: " + player.speed +
+                " m_x:" + state.mouse.x +
+                " m_y:" + state.mouse.y +
+                " p_x:" + p_abs.x +
+                " p_y:" + p_abs.y /*+
+                " t_x:" + test_dir.x +
+                " t_y:" + test_dir.y */
+                , 20, CANVAS_HEIGHT - 5);
+        }
     },
     update : (ratio) =>
     {
@@ -65,7 +141,7 @@ let player = {
         const has_moved = n_x != player.x || n_y != player.y;
         if (player.last_move == 0 && has_moved) // has not moved previously
         {
-            player.last_move = global_interval;
+            player.last_move = state.interval;
         }
         else if (player.last_move != 0 && !has_moved) // has stopped moving
         {
@@ -73,8 +149,9 @@ let player = {
         }
 
         // TODO: should prolly use some linear smoothing for this
+        // TODO: deaccel is broken
         const acc_delta = player.max_speed - player.min_speed;
-        let goal_ratio = (global_interval - player.last_move) / player.accelerate_time_ms;
+        let goal_ratio = (state.interval - player.last_move) / player.accelerate_time_ms;
         if (goal_ratio >= 1.0)
             goal_ratio = 1.0;
         if (has_moved && player.speed != player.max_speed) // if we're not in the speed cap and we're moving start accelerating
@@ -129,7 +206,8 @@ const event_game_loop = () =>
 {
     const now   = Date.now();
     const delta = now - prev;
-    global_interval += delta;
+    state.interval += delta;
+    state.m_x
 
     event_update(delta / 1000);
     event_render();
@@ -137,7 +215,7 @@ const event_game_loop = () =>
     window.requestAnimationFrame(event_game_loop);
 };
 
-const PROGRESS_TOTAL = 1;
+const PROGRESS_TOTAL = 2;
 let   load_progress  = 0;
 let   has_loaded     = false;
 
@@ -172,12 +250,30 @@ $('jswarning', (d) =>
 
     addEventListener('keydown', (e) => { keys[e.key] = true;});
     addEventListener('keyup', (e) => { delete keys[e.key]; });
+    addEventListener('mousemove', (e) =>
+    {
+        const bcr = canvas.element.getBoundingClientRect();
+        const n_x = e.clientX - bcr.left;
+        const n_y = e.clientY - bcr.top;
+        
+        state.mouse.capture = n_x >= 0 && n_x <= CANVAS_WIDTH && n_y >= 0 && n_y <= CANVAS_HEIGHT;
+        if (state.mouse.capture)
+        {
+            state.mouse.x = n_x;
+            state.mouse.y = n_y;
+        }
+
+    });
 
     // Check reso
     event_resize();
 
-    player.sprite.onload = () => { commit_progress(); };
-    player.sprite.src = './assets/sprites/nanahi_base.png';
+    player.sprite.base.onload = () => { commit_progress(); };
+    player.sprite.base.src = './assets/sprites/nanahi_base.png';
+
+    player.sprite.eyes.onload = () => { commit_progress(); };
+    player.sprite.eyes.src = './assets/sprites/nanahi_eyes.png';
+
 
     // Wait for everything to load then Trigger event loop
     console.log('Waiting for everything to load...');
